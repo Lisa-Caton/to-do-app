c5f83738a03fddaa7bfd958854061b2528744093
Lisa Caton <lisacaton01@gmail.com>
2018-01-15 12:50:19 -0500

    Adding the delete button w/out functionality

--- app.js
+++ app.js
@@ -3,30 +3,41 @@ function onReady() {
   const newToDoText = document.getElementById('newToDoText');//input
   const toDoList = document.getElementById('toDoList');//ul
 
-
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
     let title = newToDoText.value;// get the text
 
     let newLi = document.createElement('li');// creat a new li
-
+    let deleteBtn = document.createElement('input');// create a button
     let checkbox = document.createElement('input');// create a new input
 
+
     checkbox.type = "checkbox";// set the input's type to checkbox
+    deleteBtn.type = "button";// set the input's type to button
+
+    deleteBtn.value = 'delete';// Text content into the delete button
+    deleteBtn.setAttribute("id","deleteBtn");
 
     newLi.textContent = title;// set the title
 
     newLi.appendChild(checkbox);// attach the checkbox to the li
+    newLi.appendChild(deleteBtn);// attach the deleteBtn to the li
 
     toDoList.appendChild(newLi);// attach the li to the ul
 
     newToDoText.value = '';// empty the input
 
+  // document.getElementById('deleteBtn').onclick = function(){
+  //   newLi.remove('li');
+  // };//bad code = will only delete the first li???
+
   });//end addToDoForm.addEventListener
 }//end function onReady
 
 window.onload = function() {
-  alert("The window has been loaded!");
+  // alert("The window has been loaded!");
   onReady();
+
+
 };
